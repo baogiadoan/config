70143eaa1b5548a3f33a6b5225ee024a60692c8b
Bao Doan <giabaodoan1320@gmail.com>
2021-01-17 17:39:00 +1030

    update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top -c ~/.config/polybar/config &
-polybar bottom -c ~/.config/polybar/config &
-polybar top2 -c ~/.config/polybar/config &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top -c ~/.config/polybar/config &
+    MONITOR=$m polybar --reload bottom -c ~/.config/polybar/config &
+  done
+else
+  polybar --reload top &
+fi
+
